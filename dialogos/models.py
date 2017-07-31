15ba0e074bcd911ec739e94542917520cec343c8
meomancer <meomancer@gmail.com>
2017-07-31 09:14:41 +0700

    fix generic issue

--- dialogos/models.py
+++ dialogos/models.py
@@ -3,7 +3,10 @@ from datetime import datetime
 from django.db import models
 
 from django.conf import settings
-from django.contrib.contenttypes.generic import GenericForeignKey
+try:
+    from django.contrib.contenttypes.fields import GenericForeignKey
+except:
+    from django.contrib.contenttypes.generic import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 
